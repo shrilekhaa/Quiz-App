83d2720bf22c498f0b148d322ad8ebac8241d782
shrilekhaa <shriraghav18@gmail.com>
2020-11-09 11:55:53 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import questions from "./questions.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+  const [missNum, setMissNum] = useState({value:[1,2,3,4]});
+  var index;
+  
+	const handleAnswerOptionClick = (isCorrect,id_num) => {
+		if (isCorrect) {
+			setScore(score + 1);
+    }
+    index = missNum.value.indexOf(id_num);
+    if( index > -1)
+    {
+      missNum.value.splice(index,1);
+      setMissNum({value:missNum.value})
+    }    
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+    }
+  };
+
+  function handleClick(){
+    setShowScore(true);
+   }
+  
+	return (
+		<div className='app'>
+        <div>
+        {questions.map((question) => (
+            <div>     
+              <div className='question-segment'>
+					      	<div className='question-count'>
+                      <span className="set-color"><b>QUESTION {question.id}</b></span> 
+						  </div>
+						  <div className='question-text'><b>{question.questionText}</b></div>
+					    </div>
+					    <div className='answer-segment'>
+					    	{question.answerOptions.map((answerOption) => (
+							  <button
+                 onClick={() => handleAnswerOptionClick(answerOption.isCorrect,question.id)}>{answerOption.answerText}</button>
+						  ))}  
+				    	</div>
+				    </div>
+						))}
+            <hr></hr>
+            <button className="result-button" onClick={handleClick}>Get Result!</button>
+            { (showScore && missNum.value.length === 0) ? (
+            <div className='score-segment'>
+            <b>You scored {score} out of {questions.length}.</b>
+            </div>
+            ) : 
+            (((showScore && missNum.value.length != 0))? (<div><b>All questions are mandatory. Please answer {(missNum.value.join(","))} questions.</b> </div>):(null))}
+         </div>
+			 )
     </div>
-  );
+	);
 }
 
-export default App;
---
+++ src/questions.js
@@ -0,0 +1,44 @@
+const questions = [
+    {
+        id:1,
+        questionText: 'The Homolographic projection has the correct representation of',
+        answerOptions: [
+            { answerText: 'shape', isCorrect: false },
+            { answerText: 'area', isCorrect: true },
+            { answerText: 'baring', isCorrect: false },
+            { answerText: 'distance', isCorrect: false },
+        ],
+    },
+    {
+        id:2,
+        questionText: 'The latitudinal differences in pressure delineate a number of major pressure zones, which correspond with',
+        answerOptions: [
+            { answerText: 'zones of climate', isCorrect: true },
+            { answerText: 'zones of oceans', isCorrect: false },
+            { answerText: 'zones of land', isCorrect: false },
+            { answerText: 'zones of cylonic depressions', isCorrect: false },
+        ],
+    },
+    {
+        id:3,
+        questionText: 'The hazards of radiation belts include',
+        answerOptions: [
+            { answerText: 'deterioration of electronic circuits', isCorrect: false },
+            { answerText: 'damage of solar cells of spacecraft', isCorrect: false },
+            { answerText: 'adverse effect on living organisms', isCorrect: false },
+            { answerText: 'All of the above', isCorrect: true },
+        ],
+    },
+    {
+        id:4,
+        questionText: 'The great Victoria Desert is located in',
+        answerOptions: [
+            { answerText: 'Canada', isCorrect: false },
+            { answerText: 'West Africa', isCorrect: false },
+            { answerText: 'Australia', isCorrect: true },
+            { answerText: 'North America', isCorrect: false },
+        ],
+    },
+];
+
+export default questions;
